44a75184a5e17aaf51c0086d761508485ffaa82e
kadrikondi <abdulkadri42@gmail.com>
2024-07-23 14:42:01 +0100

    add get all and get one user

--- controllers/userController.js
+++ controllers/userController.js
@@ -117,3 +117,26 @@ export const LoginUser = async (req, res) => {
     });
   }
 };
+
+// get all users
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// get user by id
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -5,11 +5,15 @@ import {
   registerUser,
   homePage,
   LoginUser,
+  getAllUsers,
+  getUserById,
 } from "../controllers/userController.js";
 
 // Routes
 router.get("/home", homePage);
 router.post("/register", registerUser);
 router.post("/login", LoginUser);
+router.get("/users", getAllUsers);
+router.get("/user/:userId", getUserById);
 
 export default router;
